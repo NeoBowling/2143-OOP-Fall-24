7f3976e5bc9dc6395662adbcf92fe244fc60387e
Daniel Granados <149021699+NeoBowling@users.noreply.github.com>
2024-09-18 10:53:24 -0500

    Update main.cpp

--- Assignments/PO1/main.cpp
+++ Assignments/PO1/main.cpp
@@ -16,9 +16,14 @@
 *        go ahead and proceed with the calculations, along with a function that will change the result into the simpliest form. 
 *
 *  Usage:
-*        how to use the program if necessary
-*
-*  Files: data.txt          (list of all source files used in this program)
+*        Set the fractions with the operation you want to run in the input file
+*        For example, type 3/4 + 1/2 if you want to add those fractions together
+*        The result will be printed into the output file after the program is done running 
+* 
+*  Files:
+        data.txt
+        FractionsOOP2.cpp
+        output.txt 
 *****************************************************************************/
 #include <iostream>
 #include <fstream>
@@ -28,8 +33,8 @@ using namespace std;
 
 class Fraction{
 private:
-    int num;
-    int den;
+    int num;                                      //Initializes the variable num
+    int den;                                      //Initializes the variable den
 
    
 public:
@@ -45,7 +50,7 @@ public:
     Fraction(string f) {
         num = f[0] - 48;                          //Takes the inputted string's ASCII value and subtracts 48 to get an integer, sets this number as numerator
         den = f[2] - 48;                          //Takes the inputted string's ASCII value and subtracts 48 to get an integer, sets this number as denominator
-        Fraction(num, den);                       //Creates an instance of the class with the string coverted into integers 
+        Fraction(num, den);                       //Creates an instance of the class with the new integers coverted from the string 
     }
 
     friend ostream& operator <<(ostream& os, const Fraction& rhs) {
@@ -124,38 +129,63 @@ public:
         Returns: true if both fractions are equal otherwise it returns false 
     *****************************************************************************/
     bool operator==(const Fraction& rhs) {
-            
-            int num1 = this->num;                   //sets the numerator of the first fraction
-            int den1 = this->den;                   //sets the denominator of the first fraction
-            int num2 = rhs.num;                     //sets the numerator of the second fraction
-            int den2 = rhs.den;                     //sets the denominator of the second fraction
-            simple(num1, den1);                     //Reduces the first fraction into its simplest form
-            simple(num2, den2);                     //Reduces the second fraction into its simplest form
 
-            return (num1 == num2) && (den1 == den2);
+        int num1 = this->num;                       //sets the numerator of the first fraction
+        int den1 = this->den;                       //sets the denominator of the first fraction
+        int num2 = rhs.num;                         //sets the numerator of the second fraction
+        int den2 = rhs.den;                         //sets the denominator of the second fraction
+        simple(num1, den1);                         //Reduces the first fraction into its simplest form
+        simple(num2, den2);                         //Reduces the second fraction into its simplest form
+
+        return (num1 == num2)&&(den1 == den2);      //Compares if both simplified fractions are equal
     }
 
+    /************************************************************************************
+        Function: lcf
+        Purpose: finds the least common factor of two given numbers by using the gcd
+        Parameters: two integers 
+        Returns: the least common factor 
+    ************************************************************************************/
     int lcf(int a, int b)                           //I found this function at www.geeksforgeeks.org/cpp-program-for-program-to-find-lcm-of-two-numbers/
     {
-        return (a / gcd(a, b)) * b;
+        return (a / gcd(a, b)) * b;                 //calculates and Returns the least common factor 
     }
+
+    /************************************************************************************
+        Function: print
+        Purpose: print the fraction in the correct format to the console 
+        Parameters: N/A
+        Returns: N/A
+    ************************************************************************************/
     void print() {
-        cout << num << "/" << den;
+        cout << num << "/" << den;                  //Prints the numbers in the correct fraction format
     }
 
+    /************************************************************************************
+        Function: simple
+        Purpose: use the gcd to simplify the numbers into their lowest possible form
+        Parameters: the address of two integers 
+        Returns: N/A
+    ************************************************************************************/
      void simple(int& n, int& d) {
-        int divisor = gcd(n, d);
-        n /= divisor;
-        d /= divisor;
+        int divisor = gcd(n, d);                    //Initializes the variable divisor and sets it equal to the gcd of the given numbers 
+        n /= divisor;                               //Divides the n integer by the divisor 
+        d /= divisor;                               //Divides the d integer by the divisor
     }
 
+     /************************************************************************************
+        Function: gcd
+        Purpose: find the greatest common factor between two numbers 
+        Parameters: two integers 
+        Returns: the greatest common factor 
+    ************************************************************************************/
     int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
+        while (b != 0) {                            //a Conditional loop that keeps going while b is not 0 
+            int temp = b;                           //initializes a temporary integer with the value of b
+            b = a % b;                              //Sets b with the modulus of the values in a and b 
+            a = temp;                               //Sets a with the value in temp then loops 
         }
-        return a;
+        return a;                                   //Returns the last value set in a when the loop ends
     }
 };
 
@@ -164,45 +194,43 @@ public:
 
 int main()
 {
-    ifstream input;
-    input.open("data.txt");
-    ofstream output;
-    output.open("after.txt");
+    ifstream input;                                 //Initializes the input file
+    input.open("data.txt");                         //Opens the input file data.txt
+    ofstream output;                                //Initializes the output file 
+    output.open("output.txt");                      //Opens the output file output.txt
 
     
-    string f1, op, f2;
-    while (input >> f1 >> op >> f2) {
-        Fraction F1(f1);
-        Fraction F2(f2);
+    string f1, op, f2;                              //Initializes the variable string f1, op, f2
+    while (input >> f1 >> op >> f2) {               //Conditional loop that runs as long as there is a value to set for f1, op, and f2
+        Fraction F1(f1);                            //Creates an instance of Fraction named F1 with the string set in f1
+        Fraction F2(f2);                            //Creates an instance of Fraction named F2 with the string set in f2
 
-        Fraction result;
+        Fraction result;                            //Creates an instance of Fraction named result with nothing passed in
 
-        if (op == "+") {
-            result = F1 + F2;
+        if (op == "+") {                            //Compares the string in op with +, if the strings match then 
+            result = F1 + F2;                       //Add F1 by F2 then set the product in result
         }
-        else if (op == "-") {
-            result = F1 - F2;
+        else if (op == "-") {                       //Compares the string in op with -, if the strings match then
+            result = F1 - F2;                       //Subtract F1 by F2 then set the product in result 
         }
-        else if (op == "*") {
-            result = F1 * F2;
+        else if (op == "*") {                       //Compares the string in op with *, if the strings match then
+            result = F1 * F2;                       //Multiply F1 by F2 then set the product in result 
         }
-        else if (op == "/") {
-            result = F1 / F2;
+        else if (op == "/") {                       //Compares the string in op with /, if the strings match then
+            result = F1 / F2;                       //Divide F1 by F2 then set the product in result 
         }
-        else if (op == "=") {
-            F1 == F2;
+        else if (op == "=") {                       //Compares the string in op with =, if the strings match then
+            F1 == F2;                               //Compare F1 and F2 to see if they're equal 
         }
-        else {
-            cout << "Unknown operator: " << op << endl;
-            continue;
+        else {                                      //If the strings does not match any of the cases then 
+            cout<< "Unknown operator: "<<op<< endl; //Print error code followed the given string in op
+            continue;                               //Let the loop proceed
         }
 
-        cout << F1 << " " << op << " " << F2 << " = " << result << endl;
+        output<<F1<<" "<<op<<" "<<F2<<" = "<<result << endl; //Print to the output file the Fractions with the operator given and the result
 
     }
 
 
     return 0;
 }
-
-
