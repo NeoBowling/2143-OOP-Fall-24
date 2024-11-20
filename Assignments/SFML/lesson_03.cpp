b159c03a244ca9290742a49afecf3c5f946e0532
NeoBowling <danielgranados3752gmail.com>
2024-11-20 17:37:07 -0600

    run tests

--- Assignments/SFML/lesson_03.cpp
+++ Assignments/SFML/lesson_03.cpp
@@ -70,19 +70,19 @@ int main() {
             switch (event.key.code) {
                 case 71:  // Left arrow
                     std::cout << "Left arrow pressed!" << std::endl;
-                    circle.move(-.01f, 0.f);
+                    circle.move(-.1f, 0.f);
                     break;
                 case 72:  // Right arrow
                     std::cout << "Right arrow pressed!" << std::endl;
-                    circle.move(.01f, 0.f);
+                    circle.move(.1f, 0.f);
                     break;
                 case 73:  // Up arrow
                     std::cout << "Up arrow pressed!" << std::endl;
-                    circle.move(0.f, -.01f);
+                    circle.move(0.f, -.1f);
                     break;
                 case 74:  // Down arrow
                     std::cout << "Down arrow pressed!" << std::endl;
-                    circle.move(0.f, .01f);
+                    circle.move(0.f, .1f);
                     break;
                 default:
                     break;
